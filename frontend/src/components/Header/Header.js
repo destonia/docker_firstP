ebad41bfab02899202363bc8561e6b7edc3e7d93
tien-dev <tienhoang0605@gmail.com>
2024-08-26 17:09:47 +0900

    thanhnat add to project

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import styles from './Header.module.css'; // Import CSS Module
 function Header() {
   return (
     <header className={styles.header}>
-      <h1>Header</h1>
+      <h1>Thanh nat</h1>
     </header>
   );
 }
